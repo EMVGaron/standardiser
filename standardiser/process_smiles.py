47ae01ebfc6d960bb4babdb04ac3976d882bc7c3
bet-gregori <elisabet.gregori@gmail.com>
2018-10-30 12:09:45 +0100

    Added handling non-connected metals in disconnect

--- standardiser/process_smiles.py
+++ standardiser/process_smiles.py
@@ -7,18 +7,28 @@ timeout = -1
 
 _metal_nof = Chem.MolFromSmarts('[Li,Na,K,Rb,Cs,F,Be,Mg,Ca,Sr,Ba,Ra,Sc,Ti,V,Cr,Mn,Fe,Co,Ni,Cu,Zn,Al,Ga,Y,Zr,Nb,Mo,Tc,Ru,Rh,Pd,Ag,Cd,In,Sn,Hf,Ta,W,Re,Os,Ir,Pt,Au,Hg,Tl,Pb,Bi]~[N,n,O,o,F]')
 _metal_non = Chem.MolFromSmarts('[Al,Sc,Ti,V,Cr,Mn,Fe,Co,Ni,Cu,Zn,Y,Zr,Nb,Mo,Tc,Ru,Rh,Pd,Ag,Cd,Hf,Ta,W,Re,Os,Ir,Pt,Au]~[B,C,c,Si,P,As,Sb,S,Se,Te,Cl,Br,I,At]')
-_metals = ['Al','Sc','Ti','V','Cr','Mn','Fe','Co','Ni','Cu','Zn','Y','Zr','Nb','Mo','Tc','Ru','Rh','Pd','Ag','Cd','Hf','Ta','W','Re','Os','Ir','Pt','Au','Sn']
+_metals = ['[Al]','[Sc]','[Ti]','[V]','[Cr]','[Mn]','[Fe]','[Co]','[Ni]','[Cu]','[Zn]','[Y]','[Zr]','[Nb]','[Mo]','[Tc]','[Ru]','[Rh]','[Pd]','[Ag]','[Cd]','[Hf]','[Ta]','[W]','[Re]','[Os]','[Ir]','[Pt]','[Au]','[Sn]','[Pb]']
 
 def disconnect(mol):
     """
     Adapated from molVS standardizer module. Now it returns the list of metals it has disconnected
     """
+    metals = set([])
+    for metal_atom in _metals:
+        rwmol = Chem.RWMol(mol)
+        smarts = Chem.MolFromSmarts(metal_atom)
+        pairs = rwmol.GetSubstructMatches(smarts)
+        for i, in reversed(pairs):
+            metalSymbol = rwmol.GetAtomWithIdx(i).GetSmarts()
+            metals.add(metalSymbol)
+            rwmol.RemoveAtom(i)
+        mol = rwmol.GetMol()
+
     for smarts in [_metal_nof, _metal_non]:
         pairs = mol.GetSubstructMatches(smarts)
         rwmol = Chem.RWMol(mol)
         orders = []
-        metals = set([])
-        for i, j in pairs:
+        for i, j in reversed(pairs):
             metalSymbol = mol.GetAtomWithIdx(i).GetSmarts()
             metals.add(metalSymbol)
             orders.append(int(mol.GetBondBetweenAtoms(i, j).GetBondTypeAsDouble()))
@@ -172,7 +182,7 @@ def std(mol, returnMetals=False):
     # Standardize and return a dictionary with the smiles as keys
     # and the molecule object and whether it's a metal ion as values
     stdD = {}
-    
+
     # Check single atom compounds, to see if they are metal ions
     if mol.GetNumAtoms() == 1:
         at = mol.GetAtoms()[0].GetSymbol()
